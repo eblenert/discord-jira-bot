f8ff934c1d0967c9d528d524fd8cb311d2c71654
Erik Blenert <erik.blenert@gmail.com>
2024-07-01 00:43:09 +0300

    feat: Updated request to expect a POST.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
+router.post("/", function (req, res, next) {
   console.log(req.body);
   console.log(req.headers);
   console.log(req.query);
