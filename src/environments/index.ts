1930132c5441f536393715f649008fdb5a666041
Erik Blenert <erik.blenert@gmail.com>
2024-07-07 23:41:26 +0300

    feat: Adds dotenv and necessary updates.

--- src/environments/index.ts
+++ src/environments/index.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL,
+  DISCORD_WEBHOOK_URL: process.env.DISCORD_WEBHOOK_URL || "",
   JIRA_SECRET_KEY: process.env.JIRA_SECRET_KEY || "",
+  IS_DEV: process.env.DEV?.toLowerCase() === "true" || false,
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from "./app";
+import dotenv from "dotenv";
 
+dotenv.config();
 const initServer = async () => {
   app.listen(3000, () => {
     console.log(`Listening on port ${3000}`);
